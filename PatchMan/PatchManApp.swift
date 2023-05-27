b208bd7826c7791eff4475523519a9280ebaf357
Corey Oliphant <coreysather@gmail.com>
2023-05-26 20:39:04 -0500

    Initial Commit

---
+++ PatchMan/PatchManApp.swift
@@ -0,0 +1,17 @@
+//
+//  PatchManApp.swift
+//  PatchMan
+//
+//  Created by Corey Oliphant on 5/26/23.
+//
+
+import SwiftUI
+
+@main
+struct PatchManApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
